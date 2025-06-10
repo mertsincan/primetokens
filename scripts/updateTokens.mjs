dd4b091e8ac3a5f24bc54fb97b8d103fb7d43175
Mert Sincan <sincan.mert@gmail.com>
2025-06-10 18:34:48 +0100

    update

--- scripts/updateTokens.mjs
+++ scripts/updateTokens.mjs
@@ -2,9 +2,25 @@ import fs from 'fs';
 
 (async () => {
   try {
-    /*const response = await fetch('/tokens');
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-
+    /*
+    const response = await fetch(`${process.env.DESIGNER_API_URL}/theme/figma`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken
+      },
+      body: JSON.stringify({
+        name: themeName,
+        figma_tokens: figmaData,
+        project: 'primevue',
+        base: 'Figma',
+        config: {
+          font_size: '14px',
+          font_family: 'Inter var'
+        }
+      })
+    });
+    
     const data = await response.json();*/
     const data = `My Token: ${process.env.PRIME_TOKEN}`;
     fs.writeFileSync('demo_tokens.json', JSON.stringify(data, null, 2));
