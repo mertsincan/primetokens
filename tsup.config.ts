37a149f4ce6ec1d029bb00b2c5943d73bfed84ea
Mert Sincan <sincan.mert@gmail.com>
2025-06-10 22:27:25 +0100

    refactor: change module type and output format

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ['esm'],
+    format: ['cjs'],
     outDir: 'dist',
     external: [],
     noExternal: ['@actions/core'],
